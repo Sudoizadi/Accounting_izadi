60265beabf8f5bc668f6192ae2f5ff075debe75a
Sudoizadi <87995572+Sudoizadi@users.noreply.github.com>
2021-07-31 15:09:33 +0430

    Add files via upload

---
+++ Accounting_Pro/frmReports.cs
@@ -0,0 +1,197 @@
+﻿using Accounting.Business;
+using Accounting.DataLayer.Repository;
+using Accounting.DataLayer.Services;
+using Accounting.Utility;
+using Accounting.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accounting_Pro
+{
+    public partial class frmReports : Form
+    {
+        ICustomerRepository db;
+        public int TypeID = 0;
+        public frmReports()
+        {
+            InitializeComponent();
+            db = new CustomerRepository();
+        }
+
+        private void btn_filters_Click(object sender, EventArgs e)
+        {
+            filters();
+        }
+
+        void filters()
+        {
+            List<Accounting.DataLayer.Repository.Accounting> res = new List<Accounting.DataLayer.Repository.Accounting>();
+            DateTime? startdate;
+            DateTime? enddate;
+            
+
+            if ((int)cb_Customer.SelectedValue != 0)
+            {
+
+                int Customerid = int.Parse(cb_Customer.SelectedValue.ToString());
+                res.AddRange(db.GetAccounting(TypeID).Where(c => c.CustomerID == Customerid));
+            }
+            else
+            {
+                res.AddRange(db.GetAccounting(TypeID));
+
+            }
+            if (txt_fromdate.Text != "    /  /")
+            {
+                startdate = Convert.ToDateTime(txt_fromdate.Text.ToString());
+                startdate = Convertor.ToMiladi(startdate.Value);
+                res = res.Where(C => C.Time >= startdate.Value).ToList();
+            }
+            if (txt_todate.Text != "    /  /")
+            {
+                enddate = Convert.ToDateTime(txt_todate.Text);
+                enddate = Convertor.ToMiladi(enddate.Value);
+                res = res.Where(C => C.Time <= enddate.Value).ToList();
+
+            }
+
+            dg_Customer.AutoGenerateColumns = false;
+            dg_Customer.Rows.Clear();
+            foreach (var accounting in res)
+            {
+                string customerName = db.GetName_fromID(accounting.CustomerID);
+                dg_Customer.Rows.Add(accounting.ID, customerName, accounting.Amount.totoman(),accounting.Time.ToShamsi(),accounting.Description);
+            }
+        }
+        void report()
+        {
+            ViewModel_Report rb =computation.report();
+            lbl_pay.Text = rb.Pay;
+            lbl_recive.Text = rb.Received;
+            lbl_number.Text = rb.Number;
+        }
+            private void frmReports_Load(object sender, EventArgs e)
+        {
+            report();
+            List<ViewModel_GetName> list = new List<ViewModel_GetName>();
+            
+            
+            
+            
+            list.Add(new ViewModel_GetName()
+            
+            {
+                ID = 0,
+                FULLNAME = "انتخاب کنید"
+
+            });
+            list.AddRange(db.Filters_Name());
+            cb_Customer.DataSource = list;
+            cb_Customer.DisplayMember = "FULLNAME";
+            cb_Customer.ValueMember = "ID";
+
+            if (TypeID == 1)
+            {
+                this.Text = "درآمد ها";
+                db.GetAccounting(TypeID);
+                
+            }
+            else
+            {
+
+                this.Text = "هزینه ها";
+                
+
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            if (dg_Customer.CurrentRow != null)
+            {
+
+                int id = int.Parse(dg_Customer.CurrentRow.Cells[0].Value.ToString());
+                if (MessageBox.Show("ایا از حذف خود مطمعن هستید؟؟", "هشدار", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    db.Delete(id, "Accounting");
+                    filters();
+                }
+            }
+            else
+            {
+
+
+                MessageBox.Show("لطفا مشتری مورد نظر برای حذف کردن را انتخاب کنید", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            filters();
+        }
+
+        private void btn_edit_Click(object sender, EventArgs e)
+        {
+            if (dg_Customer.CurrentRow != null)
+            {
+
+                int id = int.Parse(dg_Customer.CurrentRow.Cells[0].Value.ToString());
+                frm_transaction frm_Transaction = new frm_transaction();
+                frm_Transaction.ID = id;
+                frm_Transaction.typeid = TypeID;
+                if (frm_Transaction.ShowDialog() == DialogResult.OK)
+                {
+                    filters();
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("لطفا مقدار مورد نظر برای ویرایش را انخاب کنید", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void cb_Customer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_print_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Customer");
+            dt.Columns.Add("Amount");
+            dt.Columns.Add("Description");
+            dt.Columns.Add("Time");
+            foreach (DataGridViewRow add in dg_Customer.Rows)
+            {
+
+                dt.Rows.Add(
+                    add.Cells[1].Value.ToString(),
+                    add.Cells[2].Value.ToString(),
+                    add.Cells[4].Value.ToString(),
+                    add.Cells[3].Value.ToString()
+                    );
+            
+            }
+            print.Load(Application.StartupPath+"/Report.mrt");
+            print.RegData("DT", dt);
+
+            print.Print();
+            
+        }
+
+        
+    }
+}
---
+++ Accounting_Pro/frm_transaction.cs
@@ -0,0 +1,225 @@
+﻿using Accounting.DataLayer.Repository;
+using Accounting.DataLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ValidationComponents;
+
+namespace Accounting_Pro
+{
+    public partial class frm_transaction : Form
+    {
+        ICustomerRepository db;
+        public int typeid = 0;
+        public int ID = 0;
+        public frm_transaction()
+        {
+            InitializeComponent();
+            db = new CustomerRepository();
+        }
+
+        private void frm_transaction_Load(object sender, EventArgs e)
+        {
+            dg_product.Visible = false;
+            txt_Description.Visible = false;
+            lbl_Description.Visible = false;
+            txt_product.Visible = false;
+            dg_person.AutoGenerateColumns = false;
+            dg_product.AutoGenerateColumns = false;
+            dg_person.DataSource = db.Filters_Name();
+            if (ID != 0)
+            {
+                if (typeid == 1)
+
+                {
+                    this.Text = "ویرایش اطلاعات دریافتی ها";
+                    var res = db.GetById(ID);
+                    rb_Received.Checked = true;
+                    rb_pay.Visible = false;
+                    btn_submit.Text = "ویرایش";
+                    txt_person.Text = db.GetName_fromID(res[0].CustomerID);
+                    txt_product.Text = res[0].Description;
+                    txt_pay.Text = res[0].Amount.ToString();
+                }
+                if (typeid == 2)
+                {
+                    this.Text = "ویرایش اطلاعات  پرداختی ها";
+                    var res = db.GetById(ID);
+                    rb_pay.Checked = true;
+                    rb_Received.Visible = false;
+                    btn_submit.Text = "ویرایش";
+                    txt_person.Text = db.GetName_fromID(res[0].CustomerID);
+                    txt_Description.Text = res[0].Description;
+                    txt_pay.Text = res[0].Amount.ToString();
+
+                }
+                //this.Text = "ویرایش اطلاعات";
+                //var res = db.GetById(ID);
+                //btn_submit.Text = "ویرایش";
+                //txt_person.Text = db.GetName_fromID(res[0].CustomerID);
+                //txt_Description.Text = res[0].Description;
+                //txt_pay.Text = res[0].Amount.ToString();
+                //if (res[0].TypeID == 1)
+                //{
+                //    rb_Received.Checked = true;
+                //}
+                //else
+                //{
+                //    rb_pay.Checked = true;
+
+                //}
+
+
+            }
+            else
+            {
+                this.Text = "افزودن اطلاعات";
+
+            }
+        }
+
+        private void txt_person_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dg_person_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_person.Text = dg_person.CurrentRow.Cells[0].Value.ToString();
+        }
+
+        private void txt_searcher_TextChanged(object sender, EventArgs e)
+        {
+            dg_person.AutoGenerateColumns = false;
+            dg_person.DataSource = db.Filters_Name(txt_searcher.Text);
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            if (BaseValidator.IsFormValid(this.components))
+            {
+                Accounting.DataLayer.Repository.Accounting accounting = new Accounting.DataLayer.Repository.Accounting()
+                {
+                    Amount = int.Parse(txt_pay.Value.ToString()),
+                    CustomerID = db.GetIDByName(txt_person.Text),
+                    Time = DateTime.Now
+
+                };
+                if (ID == 0)
+                {
+
+                    if (rb_pay.Checked)
+                    {
+                        accounting.TypeID = 2;
+                        accounting.Description = txt_Description.Text;
+                        db.Insert(accounting);
+                        DialogResult = DialogResult.OK;
+
+                    }
+                    else
+                    {
+                        accounting.TypeID = 1;
+                        accounting.Description = txt_product.Text;
+                        db.Insert(accounting);
+                        DialogResult = DialogResult.OK;
+
+                    }
+
+
+                }
+                else
+                {
+
+                    if (typeid == 1)
+                    {
+                        accounting.TypeID = 1;
+                        accounting.ID = ID;
+                        accounting.Description = txt_product.Text;
+                        db.Update(accounting);
+                        DialogResult = DialogResult.OK;
+                    }
+                    else
+                    {
+                        accounting.TypeID = 2;
+                        accounting.ID = ID;
+                        accounting.Description = txt_Description.Text;
+                        db.Update(accounting);
+                        DialogResult = DialogResult.OK;
+                    }
+
+
+                }
+
+                //if (ID != 0)
+                //{
+                //    accounting.ID = ID;
+                //    db.Update(accounting);
+                //   
+
+                //}
+                //else
+                //{
+                //    if (rb_pay.Checked || rb_Received.Checked)
+                //    {
+
+
+                //        db.Insert(accounting);
+                //       
+                //    }
+                //    else
+                //    {
+
+                //        MessageBox.Show("لطفا نوع تراکنش را وارد کنید ", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+        }
+
+
+        private void rb_Received_CheckedChanged(object sender, EventArgs e)
+        {
+            lbl_Description.Visible = true;
+            rvalid.ErrorMessage = "لطفا نام محصول را از لیست زیر وارد کنید";
+            rvalid.ControlToValidate = txt_product;
+            lbl_Description.Text = "عنوان کالا:";
+            txt_product.Visible = true;
+            dg_product.Visible = true;
+            dg_product.DataSource = db.GetProducts("Filters_Name");
+            //dg_product.AutoGenerateColumns = false;
+
+            txt_Description.Visible = false;
+
+        }
+
+        private void rb_pay_CheckedChanged(object sender, EventArgs e)
+        {
+            rvalid.ErrorMessage = "لطفا شرح هزینه را وارد کنید";
+            rvalid.ControlToValidate = txt_Description;
+            lbl_Description.Visible = true;
+            lbl_Description.Text = "شرح:";
+            txt_product.Visible = false;
+            dg_product.Visible = false;
+
+            txt_Description.Visible = true;
+
+        }
+
+
+
+        private void dg_person_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dg_product_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_product.Text = dg_product.CurrentRow.Cells[0].Value.ToString();
+        }
+    }
+}
