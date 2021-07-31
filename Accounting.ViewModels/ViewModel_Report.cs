d320628010b653dfd4fb130f343db2ba3257ecd8
Sudoizadi <87995572+Sudoizadi@users.noreply.github.com>
2021-07-31 15:04:49 +0430

    Add files via upload

---
+++ Accounting.ViewModels/ViewModel_Report.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.ViewModels
+{
+    public class ViewModel_Report
+    {
+
+        public string Received { get; set; }
+        public string Pay { get; set; }
+        public string Number { get; set; }
+        public string Remaining { get; set; }
+
+
+    }
+}
